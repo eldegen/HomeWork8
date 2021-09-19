905c3c5cc181aa466f1efb8cd4d8b441d9bc47d0
eldegen <89722414+eldegen@users.noreply.github.com>
2021-09-19 20:39:52 +0600

    Дополнен персонаж Magic

--- src/kg/geektech/game/players/Magic.java
+++ src/kg/geektech/game/players/Magic.java
@@ -1,5 +1,7 @@
 package kg.geektech.game.players;
 
+import java.util.Random;
+
 public class Magic extends Hero {
     public Magic(int health, int damage) {
         super(health, damage, SuperAbility.BOOST);
@@ -7,6 +9,14 @@ public class Magic extends Hero {
 
     @Override
     public void applySuperPower(Hero[] heroes, Boss boss) {
+        Random rand = new Random();
+        int dmgBoost = rand.nextInt(5);
 
+        for (int i = 0; i < heroes.length; i++) {
+            heroes[i].setDamage(heroes[i].getDamage() + dmgBoost);
+        }
+        System.out.println("================= Magic's boost! =================");
+        System.out.println("Magic buffed to all heroes " + dmgBoost + " DMG");
+        System.out.println("==================================================");
     }
 }
